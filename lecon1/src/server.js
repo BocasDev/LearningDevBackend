7eb0f0203a5e9f8c72a9bfc20f74d6f0958d23b6
Boc@s <hodboc@gmail.com>
2026-08-01 14:47:08 +0100

    ajout exercice1

--- lecon1/src/server.js
+++ lecon1/src/server.js
@@ -2,11 +2,27 @@ const express=require("express");
 const app= express();
 const PORT=3000;
 app.use(express.json());
-app.get("/", (req,res) => {
+app.get("/api/info", (req,res) => {
+    const resultat=`
+    "nom":"Learning-node",
+    "version":"1.0",
+     "auteur":"Symphorien"`;
     res.json({
-        message:"Backend API opérationnel"
+        message:"Backend API opérationnel"   
     });
+    console.log(resultat);
 });
+app.get("/api/test", (req,res)=>{
+    const op=`
+    "status": "Ok",
+    "database": "PostgreSQL bientot connecté"
+    `;
+    res.json({
+        message:"test réussi"
+    });
+    console.log(op);
+    
+})
 app.listen(PORT, ()=>{
     console.log(`Serveur lancé sur le port ${PORT}`);
     
